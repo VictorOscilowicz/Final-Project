e5d757811ba0dc58d0c1b7d6e21fd436cc0611ef
VictorOscilowicz <7voscilowicz@stpatrick.org>
2016-12-12 10:54:11 -0600

    Add files via upload
    I began work on the item class.

---
+++ item.js
@@ -0,0 +1,13 @@
+class Item{
+  //Fill in the item class below this comment.
+  constructor(itemname,itemprice,itemshipping){
+    this.name=itemname;
+    this.price=itemprice;
+    this.shipping=itemshipping;
+  }
+}
+
+//Create your three test items below this comment.
+var computer = new Item ("Dell Laptop",899.99,10);
+var ball = new Item ("Soccer Ball",35.99,6);
+var tool = new Item ("Screwdriver",46.99,6);
