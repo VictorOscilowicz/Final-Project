b2b09fb5b6f6e3a9a69aee1909203aebbd4a620a
VictorOscilowicz <7voscilowicz@stpatrick.org>
2016-12-16 15:16:31 -0600

    Add files via upload

--- password.js
+++ password.js
@@ -7,29 +7,27 @@ class Password{
 
   //Instance functions below this comment.
   validPublicKey(){
-    while(this.publicKey.length>=8; this.publicKey.length<=25);
+    while(this.publicKey.length>=8 && this.publicKey.length<=25);
       return "true";
     }
     else(){
       return "false";
     }
-  }
 
   validPrivateKey(){
-    for(var h){
-
-      if(){
-
+    for(var h=0;h<this.privateKy.length;a++){
+      if(this.privateKey[4]=="-" && this.privateKey[9]=="-"){
+        return "true";
       }
+    }
 
-      else if(){
+      else if(this.privateKey.length!==""){
         return "true";
       }
 
       else(){
         return "false";
       }
-    }
   }
 
   //Static function below this comment.
@@ -38,8 +36,9 @@ class Password{
     let limit=14;
 
     for(l=0;l<=14;l++){
-      let number = Math.round(Math.random()*10);
-
+      let number=String(Math.round(Math.random()*10));
+      key = key + number;
+      number.push[4,9]=="-";
     }
   }
 }
